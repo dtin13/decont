f87b1ce421d37987510c13ae0134ffbfcf5f05b8
Agustín García <dtin13@gmail.com>
2019-11-18 19:03:39 +0100

    download script created

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,2 +1,16 @@
 # This script should download the file specified in the first argument ($1), place it in the directory specified in the second argument, 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
+
+echo "Descarga de los archivos de secuencia..."
+for url in $(cat data/urls)
+do
+wget -P data $url
+echo "el archivo $url se descargó correctamente"
+echo
+done 
+
+echo "Descarga de la base de datos de contaminantes"
+wget -O res/contaminants.fasta.gz https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz
+echo "Descompresión del fichero de contaminantes..."
+gunzip -k res/contaminants.fasta.gz
+
